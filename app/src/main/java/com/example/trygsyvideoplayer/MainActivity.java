ba57ddd07f1eb1b4db5961e602d0448178ce0762
Chongyu Wang <181830175@smail.nju.edu.cn>
2020-07-29 20:36:39 +0800

    feat:新增RecyclerView

--- app/src/main/java/com/example/trygsyvideoplayer/MainActivity.java
+++ app/src/main/java/com/example/trygsyvideoplayer/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.trygsyvideoplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,31 +20,62 @@ import com.shuyu.gsyvideoplayer.GSYVideoManager;
 import com.shuyu.gsyvideoplayer.utils.OrientationUtils;
 import com.shuyu.gsyvideoplayer.video.StandardGSYVideoPlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     StandardGSYVideoPlayer videoPlayer;
 
     OrientationUtils orientationUtils;
 
+    List<Video> mVideoList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Video video = VideoStub.getVideo(0);
+        RecyclerView videoRecyclerView = (RecyclerView)findViewById(R.id.videoRecyclerView);
+        mVideoList = new ArrayList<>();
+        for(int index = 0;index < VideoStub.getLength();index++){
+            mVideoList.add(VideoStub.getVideo(index));
+            if(index>255){
+                break;
+            }
+        }
 
-        TextView textView=findViewById(R.id.text);
-        View btn = findViewById(R.id.watch);
+//        Video video = VideoStub.getVideo(0);
+//        TextView textView=findViewById(R.id.text);
+//        View btn = findViewById(R.id.watch);
+//
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, GSYPlayerActivity.class);
+//                intent.putExtra("videoUrl",video.videoUrl);
+//                startActivity(intent);
+//            }
+//        });
+//        textView.setText(video.toString());
 
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        videoRecyclerView.setLayoutManager(layoutManager);
+        VideoAdapter adapter = new VideoAdapter(mVideoList);
+        videoRecyclerView.setAdapter(adapter);
+
+        adapter.setOnitemClickLintener(new VideoAdapter.OnitemClick() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, GSYPlayerActivity.class);
-                intent.putExtra("videoUrl",video.videoUrl);
-                startActivity(intent);
+            public void onItemClick(String videoURL) {
+                Intent it = new Intent(MainActivity.this, GSYPlayerActivity.class);
+                it.putExtra("videoUrl", videoURL);
+                startActivity(it);
             }
         });
-        textView.setText(video.toString());
-
     }
 
 }
